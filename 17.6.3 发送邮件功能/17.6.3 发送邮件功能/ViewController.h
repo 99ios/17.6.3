cf47a67c750a4fc2eba8a17888d72b240d1dca7b
test <422538722@qq.com>
2017-04-16 16:57:51 +0800

    17.6.3
    发送邮件功能

---
+++ 17.6.3 发送邮件功能/17.6.3 发送邮件功能/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  17.6.3 发送邮件功能
+//
+//  Created by 李维佳 on 2017/4/14.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 17.6.3 发送邮件功能/17.6.3 发送邮件功能/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  17.6.3 发送邮件功能
+//
+//  Created by 李维佳 on 2017/4/14.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
